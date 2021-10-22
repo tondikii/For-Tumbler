c8fae9b3d5581c2f559afdd27d50bb1ffc7e2500
Johannes Wijaya <hannesuw@gmail.com>
2021-10-22 12:56:54 +0700

    Johannes

--- mainpage.js
+++ mainpage.js
@@ -1,248 +1,256 @@
 const lagu = {
-    loveSong: [
-        {
-            judul: "Let My Baby Stay",
-            penyanyi: "Mac Demarco",
-            link: "https://open.spotify.com/embed/track/7nxi8DUA9Iw6nnpGN3M9Zd?utm_source=generator"
-    
-        },
-        {
-            judul: "See You Again",
-            penyanyi: "Tyler, the Creator",
-            link: "https://open.spotify.com/embed/track/7KA4W4McWYRpgf0fWsJZWB?utm_source=generator"
-    
-        },
-        {
-            judul: "Baby I'm Yours",
-            penyanyi: "Breakbot, Irfane",
-            link: "https://open.spotify.com/embed/track/4qPTaehSZWKeaIgiIyprLs?utm_source=generator"
-        },
-        {
-            judul: "Shiver",
-            penyanyi: "Coldplay",
-            link: "https://open.spotify.com/embed/track/0qksx8mV28lztYIZ1om8ml?utm_source=generator"
-        },
-        {
-            judul: "505",
-            penyanyi: "Arctic Monkeys",
-            link: "https://open.spotify.com/embed/track/0BxE4FqsDD1Ot4YuBXwAPp?utm_source=generator"
-        }
-    ],
-    relax: [
-        {
-            judul: "Shouldn't be",
-            penyanyi: "Luke Chiang",
-            link: "https://open.spotify.com/embed/track/3wefloF3t1sFZx8YMFhqYB",
-        },
-        {
-            judul: "Too Good",
-            penyanyi: "Christian Kuria",
-            link: "https://open.spotify.com/embed/track/3GP7l2bmXT6T2FNAzERwBr",
-        },
-        {
-            judul: "Calvin's Joint",
-            penyanyi: "Mac Ayres",
-            link: "https://open.spotify.com/embed/track/33VQJLL7QWS0LUuF7r39Rq",
-        },
-        {
-            judul: "Slow Down",
-            penyanyi: "Mac Ayres",
-            link: "https://open.spotify.com/embed/track/3XstzgzP0rp3bzElEnRVHv",
-        },
-        {
-            judul: "Roses",
-            penyanyi: "Mac Ayres",
-            link: "https://open.spotify.com/embed/track/0ABtEGkZgxAnJOCKVVhr29",
-        },
-        {
-            judul: "Easily",
-            penyanyi: "Bruno Major",
-            link: "https://open.spotify.com/embed/track/2k9N4caeCIJLOWwWwssrEM",
-        },
-        {
-            judul: "Still",
-            penyanyi: "Jeff Bernat",
-            link: "https://open.spotify.com/embed/track/6sv7ZmasB6qSiF8hWJNpDG",
-        },
-    ],
-    
-    happy: [
-        {
-            judul: "Frank sinatra",
-            penyanyi: "Fly me to the moon",
-            link: "https://open.spotify.com/embed/track/7FXj7Qg3YorUxdrzvrcY25?utm_source=generator" ,
-          },
-          {
-              judul: "TWICE",
-              penyanyi: "What is Love",
-              link: "https://open.spotify.com/embed/track/3zhbXKFjUDw40pTYyCgt1Y?utm_source=generator" ,
-          },
-          {
-            judul: "Nat king Cole",
-            penyanyi: ".LO.V.E",
-            link: "https://open.spotify.com/embed/track/4QxDOjgpYtQDxxbWPuEJOy?utm_source=generator",
-          },
-          {
-            judul: "Payung Teduh",
-            penyanyi: "AKAD",
-            link: "https://open.spotify.com/embed/track/3AAAGS7iM1ekDywqdYMJG2?utm_source=generator",
-          },
-          {
-            judul: "Ten2Five",
-            penyanyi: "I will fly",
-            link: "https://open.spotify.com/embed/track/2W5okrcUdy35nWqhY9Oy6J?utm_source=generator",
-          },
-          {
-            judul: "GAC",
-            penyanyi: "Bahagia",
-            link: "https://open.spotify.com/embed/track/1FqA1hu2s1M9sNiZBy81T4?utm_source=generator",
-          }
-    ],
-    gloomy: [
-        {
-            judul: "Terpaut Oleh Waktu",
-            penyanyi: "Danilla",
-            link: "https://open.spotify.com/embed/track/6d3iUsWvahrR51nAfx50DL",
-        },
-        {
-            judul: "Pesan Dibalik Awan",
-            penyanyi: "Adithia Sofyan",
-            link: "https://open.spotify.com/embed/track/6GseyHjulIwmuNrs54rIie",
-        },
-        {
-            judul: "Di Senayan",
-            penyanyi: "Ardhito Pramono",
-            link: "https://open.spotify.com/embed/track/3UM15EeyHsRCbbIym5bmIt",
-        },
-        {
-            judul: "Hitam Putih",
-            penyanyi: "Fourtwnty",
-            link: "https://open.spotify.com/embed/track/0O6g2QFUhuLqRzGqhyzrIu",
-        },
-        {
-            judul: "Kusut",
-            penyanyi: "Foutwnty",
-            link: "https://open.spotify.com/embed/track/3diD8b3u6oEn74BezQpujj",
-        },
-        {
-            judul: "Resah",
-            penyanyi: "Payung Teduh",
-            link: "https://open.spotify.com/embed/track/1k5vv2ood14aa5uBYWBlWh",
-        }
-    ],    
-    sad: [
-        {
-            judul: "Seventeen",
-            penyanyi: "Ayah",
-            link: "https://open.spotify.com/embed/track/3CN43LtX1MJNByRu2Yc32I?utm_source=generator" ,
-        },
-        {
-            judul: "Potret",
-            penyanyi: "Bunda",
-            link: "https://open.spotify.com/embed/track/0y2xLzlrjdi5bFIAKBVYfX?utm_source=generator",
-        },
-        {
-            judul: "Alicia",
-            penyanyi: "if aint got you",
-            link: "https://open.spotify.com/embed/track/3XVBdLihbNbxUwZosxcGuJ?utm_source=generator",
-        },
-        {
-            judul: "Bernard Park",
-            penyanyi: "The veiled Path",
-            link: "https://open.spotify.com/embed/track/0Ct2R1hzWYzzHgMhwzGdGw?utm_source=generator"
-        },
-        {
-            judul: " Fiersa Bersari",
-            penyanyi: "Waktu yg salah",
-            link: "https://open.spotify.com/embed/track/4qgIQcskrvj6nlypTvBm5k?utm_source=generator",
-        },
-    ]
+  loveSong: [
+    {
+      judul: "Let My Baby Stay",
+      penyanyi: "Mac Demarco",
+      link: "https://open.spotify.com/embed/track/7nxi8DUA9Iw6nnpGN3M9Zd?utm_source=generator",
+    },
+    {
+      judul: "See You Again",
+      penyanyi: "Tyler, the Creator",
+      link: "https://open.spotify.com/embed/track/7KA4W4McWYRpgf0fWsJZWB?utm_source=generator",
+    },
+    {
+      judul: "Baby I'm Yours",
+      penyanyi: "Breakbot, Irfane",
+      link: "https://open.spotify.com/embed/track/4qPTaehSZWKeaIgiIyprLs?utm_source=generator",
+    },
+    {
+      judul: "Shiver",
+      penyanyi: "Coldplay",
+      link: "https://open.spotify.com/embed/track/0qksx8mV28lztYIZ1om8ml?utm_source=generator",
+    },
+    {
+      judul: "505",
+      penyanyi: "Arctic Monkeys",
+      link: "https://open.spotify.com/embed/track/0BxE4FqsDD1Ot4YuBXwAPp?utm_source=generator",
+    },
+  ],
+  relax: [
+    {
+      judul: "Shouldn't be",
+      penyanyi: "Luke Chiang",
+      link: "https://open.spotify.com/embed/track/3wefloF3t1sFZx8YMFhqYB",
+    },
+    {
+      judul: "Too Good",
+      penyanyi: "Christian Kuria",
+      link: "https://open.spotify.com/embed/track/3GP7l2bmXT6T2FNAzERwBr",
+    },
+    {
+      judul: "Calvin's Joint",
+      penyanyi: "Mac Ayres",
+      link: "https://open.spotify.com/embed/track/33VQJLL7QWS0LUuF7r39Rq",
+    },
+    {
+      judul: "Slow Down",
+      penyanyi: "Mac Ayres",
+      link: "https://open.spotify.com/embed/track/3XstzgzP0rp3bzElEnRVHv",
+    },
+    {
+      judul: "Roses",
+      penyanyi: "Mac Ayres",
+      link: "https://open.spotify.com/embed/track/0ABtEGkZgxAnJOCKVVhr29",
+    },
+    {
+      judul: "Easily",
+      penyanyi: "Bruno Major",
+      link: "https://open.spotify.com/embed/track/2k9N4caeCIJLOWwWwssrEM",
+    },
+    {
+      judul: "Still",
+      penyanyi: "Jeff Bernat",
+      link: "https://open.spotify.com/embed/track/6sv7ZmasB6qSiF8hWJNpDG",
+    },
+  ],
+
+  happy: [
+    {
+      judul: "Frank sinatra",
+      penyanyi: "Fly me to the moon",
+      link: "https://open.spotify.com/embed/track/7FXj7Qg3YorUxdrzvrcY25?utm_source=generator",
+    },
+    {
+      judul: "TWICE",
+      penyanyi: "What is Love",
+      link: "https://open.spotify.com/embed/track/3zhbXKFjUDw40pTYyCgt1Y?utm_source=generator",
+    },
+    {
+      judul: "Nat king Cole",
+      penyanyi: ".LO.V.E",
+      link: "https://open.spotify.com/embed/track/4QxDOjgpYtQDxxbWPuEJOy?utm_source=generator",
+    },
+    {
+      judul: "Payung Teduh",
+      penyanyi: "AKAD",
+      link: "https://open.spotify.com/embed/track/3AAAGS7iM1ekDywqdYMJG2?utm_source=generator",
+    },
+    {
+      judul: "Ten2Five",
+      penyanyi: "I will fly",
+      link: "https://open.spotify.com/embed/track/2W5okrcUdy35nWqhY9Oy6J?utm_source=generator",
+    },
+    {
+      judul: "GAC",
+      penyanyi: "Bahagia",
+      link: "https://open.spotify.com/embed/track/1FqA1hu2s1M9sNiZBy81T4?utm_source=generator",
+    },
+  ],
+  gloomy: [
+    {
+      judul: "Terpaut Oleh Waktu",
+      penyanyi: "Danilla",
+      link: "https://open.spotify.com/embed/track/6d3iUsWvahrR51nAfx50DL",
+    },
+    {
+      judul: "Pesan Dibalik Awan",
+      penyanyi: "Adithia Sofyan",
+      link: "https://open.spotify.com/embed/track/6GseyHjulIwmuNrs54rIie",
+    },
+    {
+      judul: "Di Senayan",
+      penyanyi: "Ardhito Pramono",
+      link: "https://open.spotify.com/embed/track/3UM15EeyHsRCbbIym5bmIt",
+    },
+    {
+      judul: "Hitam Putih",
+      penyanyi: "Fourtwnty",
+      link: "https://open.spotify.com/embed/track/0O6g2QFUhuLqRzGqhyzrIu",
+    },
+    {
+      judul: "Kusut",
+      penyanyi: "Foutwnty",
+      link: "https://open.spotify.com/embed/track/3diD8b3u6oEn74BezQpujj",
+    },
+    {
+      judul: "Resah",
+      penyanyi: "Payung Teduh",
+      link: "https://open.spotify.com/embed/track/1k5vv2ood14aa5uBYWBlWh",
+    },
+  ],
+  sad: [
+    {
+      judul: "Seventeen",
+      penyanyi: "Ayah",
+      link: "https://open.spotify.com/embed/track/3CN43LtX1MJNByRu2Yc32I?utm_source=generator",
+    },
+    {
+      judul: "Potret",
+      penyanyi: "Bunda",
+      link: "https://open.spotify.com/embed/track/0y2xLzlrjdi5bFIAKBVYfX?utm_source=generator",
+    },
+    {
+      judul: "Alicia",
+      penyanyi: "if aint got you",
+      link: "https://open.spotify.com/embed/track/3XVBdLihbNbxUwZosxcGuJ?utm_source=generator",
+    },
+    {
+      judul: "Bernard Park",
+      penyanyi: "The veiled Path",
+      link: "https://open.spotify.com/embed/track/0Ct2R1hzWYzzHgMhwzGdGw?utm_source=generator",
+    },
+    {
+      judul: " Fiersa Bersari",
+      penyanyi: "Waktu yg salah",
+      link: "https://open.spotify.com/embed/track/4qgIQcskrvj6nlypTvBm5k?utm_source=generator",
+    },
+  ],
 };
-function listGloomy (lagu) {
-    let result = '';
-    for (let i in lagu.gloomy) {
-        result += `<li>
+
+let iframe = document.querySelector("iframe#musicBar");
+function runMusic(param) {
+  iframe.setAttribute("src", param);
+}
+
+function listGloomy(lagu) {
+  let result = "";
+  for (let i in lagu.gloomy) {
+    result += `<li >
         <div class="music">
           <div class="images">
-            <img src="img/isyana.jpeg" alt="" class="album" />
-            <img
-              src=${lagu.gloomy[i].link}
-            />
+          <img src="https://source.unsplash.com/random/500x500?v=1" alt="" class="album" />
+          <img
+            src="https://img.icons8.com/fluency/48/000000/plus.png"
+            class="play-button"
+          />
           </div>
           <div class="description">
             <h1>${lagu.gloomy[i].penyanyi}</h1>
             <p>${lagu.gloomy[i].judul}</p>
           </div>
           <div class="cta">
-            <div class="play-button">
-              <img src="img/play-button.png" alt="" />
+            <div class="play-button" >
+              <img src="img/play-button.png" onclick = "runMusic('${lagu.gloomy[i].link}')" class="play-music"/>
             </div>
           </div>
         </div>
-      </li>`
-    }
-    return result;
+      </li>`;
+  }
+  return result;
 }
-function listSad (lagu) {
-    let result = '';
-    for (let i in lagu.sad) {
-        result += `<li>
-        <div class="music">
-          <div class="images">
-            <img src="img/isyana.jpeg" alt="" class="album" />
-            <img
-              src=${lagu.sad[i].link}
-            />
-          </div>
-          <div class="description">
-            <h1>${lagu.sad[i].penyanyi}</h1>
-            <p>${lagu.sad[i].judul}</p>
-          </div>
-          <div class="cta">
-            <div class="play-button">
-              <img src="img/play-button.png" alt="" />
-            </div>
-          </div>
+function listSad(lagu) {
+  let result = "";
+  for (let i in lagu.sad) {
+    result += `<li>
+    <div class="music">
+      <div class="images">
+      <img src="https://source.unsplash.com/random/500x500?v=1" alt="" class="album" />
+      <img
+        src="https://img.icons8.com/fluency/48/000000/plus.png"
+        class="play-button"
+      />
+      </div>
+      <div class="description">
+        <h1>${lagu.sad[i].penyanyi}</h1>
+        <p>${lagu.sad[i].judul}</p>
+      </div>
+      <div class="cta">
+        <div class="play-button">
+        <img src="img/play-button.png" onclick = "runMusic('${lagu.sad[i].link}')" class="play-music"/>
         </div>
-      </li>`
-    }
-    return result;
+      </div>
+    </div>
+  </li>`;
+  }
+  return result;
 }
-function listRelax (lagu) {
-    let result = '';
-    for (let i in lagu.relax) {
-        result += `<li>
-        <div class="music">
-          <div class="images">
-            <img src="img/isyana.jpeg" alt="" class="album" />
-            <img
-              src=${lagu.relax[i].link}
-            />
-          </div>
-          <div class="description">
-            <h1>${lagu.relax[i].penyanyi}</h1>
-            <p>${lagu.relax[i].judul}</p>
-          </div>
-          <div class="cta">
-            <div class="play-button">
-              <img src="img/play-button.png" alt="" />
-            </div>
-          </div>
+function listRelax(lagu) {
+  let result = "";
+  for (let i in lagu.relax) {
+    result += `<li>
+    <div class="music">
+      <div class="images">
+      <img src="https://source.unsplash.com/random/500x500?v=1" alt="" class="album" />
+      <img
+        src="https://img.icons8.com/fluency/48/000000/plus.png"
+        class="play-button"
+      />
+      </div>
+      <div class="description">
+        <h1>${lagu.relax[i].penyanyi}</h1>
+        <p>${lagu.relax[i].judul}</p>
+      </div>
+      <div class="cta">
+        <div class="play-button">
+        <img src="img/play-button.png" onclick = "runMusic('${lagu.relax[i].link}')" class="play-music"/>
         </div>
-      </li>`
-    }
-    return result;
+      </div>
+    </div>
+  </li>`;
+  }
+  return result;
 }
-function listHappy (lagu) {
-    let result = '';
-    for (let i in lagu.happy) {
-        result += `<li>
+function listHappy(lagu) {
+  let result = "";
+  for (let i in lagu.happy) {
+    result += `<li>
         <div class="music">
           <div class="images">
-            <img src="img/isyana.jpeg" alt="" class="album" />
-            <img
-              src=${lagu.happy[i].link}
-            />
+          <img src="https://source.unsplash.com/random/500x500?v=1" alt="" class="album" />
+          <img
+            src="https://img.icons8.com/fluency/48/000000/plus.png"
+            class="play-button"
+          />
           </div>
           <div class="description">
             <h1>${lagu.happy[i].penyanyi}</h1>
@@ -250,24 +258,25 @@ function listHappy (lagu) {
           </div>
           <div class="cta">
             <div class="play-button">
-              <img src="img/play-button.png" alt="" />
+            <img src="img/play-button.png" onclick = "runMusic('${lagu.happy[i].link}')" class="play-music"/>
             </div>
           </div>
         </div>
-      </li>`
-    }
-    return result;
+      </li>`;
+  }
+  return result;
 }
-function listLoveSong (lagu) {
-    let result = '';
-    for (let i in lagu.loveSong) {
-        result += `<li>
+function listLoveSong(lagu) {
+  let result = "";
+  for (let i in lagu.loveSong) {
+    result += `<li>
         <div class="music">
           <div class="images">
-            <img src="img/isyana.jpeg" alt="" class="album" />
-            <img
-              src=${lagu.loveSong[i].link}
-            />
+          <img src="https://source.unsplash.com/random/500x500?v=1" alt="" class="album" />
+          <img
+            src="https://img.icons8.com/fluency/48/000000/plus.png"
+            class="play-button"
+          />
           </div>
           <div class="description">
             <h1>${lagu.loveSong[i].penyanyi}</h1>
@@ -275,23 +284,23 @@ function listLoveSong (lagu) {
           </div>
           <div class="cta">
             <div class="play-button">
-              <img src="img/play-button.png" alt="" />
+            <img src="img/play-button.png" onclick = "runMusic('${lagu.loveSong[i].link}')" class="play-music"/>
             </div>
           </div>
         </div>
-      </li>`
-    }
-    return result;
+      </li>`;
+  }
+  return result;
 }
 console.log(lagu.loveSong);
-const listLagu = document.getElementById('content');
+const listLagu = document.getElementById("content");
 
-const moodRange = document.getElementById('moodrange');
-moodRange.addEventListener('change', () => {
-    // alert(moodRange.value);
-        if (moodRange.value == 0) listLagu.innerHTML = listGloomy(lagu);
-        else if (moodRange.value == 1) listLagu.innerHTML = listSad(lagu);
-        else if (moodRange.value == 2) listLagu.innerHTML = listRelax(lagu);
-        else if (moodRange.value == 3) listLagu.innerHTML = listHappy(lagu);
-        else listLagu.innerHTML = listLoveSong(lagu);
-})
+const moodRange = document.getElementById("moodrange");
+moodRange.addEventListener("change", () => {
+  // alert(moodRange.value);
+  if (moodRange.value == 0) listLagu.innerHTML = listGloomy(lagu);
+  else if (moodRange.value == 1) listLagu.innerHTML = listSad(lagu);
+  else if (moodRange.value == 2) listLagu.innerHTML = listRelax(lagu);
+  else if (moodRange.value == 3) listLagu.innerHTML = listHappy(lagu);
+  else listLagu.innerHTML = listLoveSong(lagu);
+});
